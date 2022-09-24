8667eda28a748ce9e0d56c63b818c8efd1a1e6d2
wang-yonglin <yonglin.wang@casstime.com>
2022-09-24 22:14:35 +0800

    添加pull脚本

---
+++ pull.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+cd $1
+for file in `ls ./`
+do
+echo "**************************************************************"
+if [ -d "$file" ]
+then
+  echo "开始 拉取 $file 工程"
+  cd $file
+  git pull
+  echo "$file 工程拉取完成"
+  cd ../
+elif [ -f "$file" ]
+then
+  echo "$file 是一个文件，跳过"
+fi
+echo ""
+echo ""
+done
